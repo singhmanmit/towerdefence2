5daf881d8bb221805f539387de33eb58048db33f
Floor5Studio <danitl150@gmail.com>
2014-08-12 17:19:11 -0700

    new camera view
    This update has a new look of the camera and involves the use of a ray
    cast from the center of the screen, this will be used later for the
    characters shooting direction.

---
+++ Assets/Scripts/Daniel Tay/CameraRay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRay : MonoBehaviour {
+
+	public Transform cube;
+	public float raycastDist;
+	public string tagCheck = "Level Parts";
+	public string enemCheck = "enemy";
+	public bool checkAllTags = false;
+
+	void Update() {
+
+		bool foundHit = false;
+
+		RaycastHit hit = new RaycastHit();
+
+		foundHit = Physics.Raycast(transform.position, transform.forward,out hit);
+
+//		if (foundHit && !checkAllTags && hit.transform.tag != tagCheck )
+//			foundHit = false;
+
+//		if (foundHit)
+//		{
+			cube.position = hit.point;
+			cube.rotation = Quaternion.LookRotation (hit.normal);
+//		}
+	}
+}
