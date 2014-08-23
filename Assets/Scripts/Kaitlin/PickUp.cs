7ba22779f0b7c47b72fc2db24793e96f17c9664f
Kaitlin <kaitlin.chin.713>
2014-08-22 21:41:11 -0700

    kat added pickup prefabs and picks up work. just need actual stuff to make sure they do what they are suppose to do

--- Assets/Scripts/Kaitlin/PickUp.cs
+++ Assets/Scripts/Kaitlin/PickUp.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class PickUp : MonoBehaviour 
 {
+	public float health;
+
 	// Use this for initialization
 	void Start () 
 	{
-	
+		health = 20.0f;	
 	}
 	
 	// Update is called once per frame
@@ -19,10 +21,23 @@ public class PickUp : MonoBehaviour
 	{
 		if(collider.gameObject.tag == "Player")
 		{
-			if(this.gameObject.tag == "healthpickup")
+			if(this.gameObject.tag == "health")
 			{
+				collider.SendMessage("addHealth", health, SendMessageOptions.DontRequireReceiver);
 				Destroy(this.gameObject);
 				Debug.Log("You picked up health!");
+			}
+
+			if(this.gameObject.tag == "power")
+			{
+				Destroy (this.gameObject);
+				Debug.Log ("You picked up a power up!");
+			}
+
+			if(this.gameObject.tag == "currency")
+			{
+				Destroy(this.gameObject);
+				Debug.Log ("You picked up money!");
 			}
 		}
 	}
