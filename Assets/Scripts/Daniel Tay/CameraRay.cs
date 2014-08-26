a50e23873fd07ab717e9d4f6d79e99516a8bdff8
daniTaySTD <danitl150@gmail.com>
2014-08-26 11:26:03 -0700

    changed the camera raycast script a bit. Raycast works like a charm on scenes 1.2 and rootlevel, but has bufs on scenes 1.0 and 1.1, trying to trouble shoot the issue

--- Assets/Scripts/Daniel Tay/CameraRay.cs
+++ Assets/Scripts/Daniel Tay/CameraRay.cs
@@ -5,25 +5,45 @@ public class CameraRay : MonoBehaviour {
 
 	public Transform cube;
 	public float raycastDist;
-	public string tagCheck = "Level Parts";
-	public string enemCheck = "enemy";
-	public bool checkAllTags = false;
 
 	void Update() {
 
-		bool foundHit = false;
-
-		RaycastHit hit = new RaycastHit();
-
-		foundHit = Physics.Raycast (transform.position, transform.forward, out hit);
-
+//		bool foundHit = false;
+//
+//		RaycastHit hit = new RaycastHit();
+//
+//		foundHit = Physics.Raycast (transform.position, transform.forward, out hit);
+//
 //		if (foundHit && !checkAllTags && hit.transform.tag != tagCheck )
 //			foundHit = false;
 //
-//		if (foundHit)
+//		if (foundHit = true)
 //		{
+//			cube.position = hit.point;
+//			cube.rotation = Quaternion.LookRotation (hit.normal);
+//		}
+
+		Raycasting ();
+	}
+
+	void Raycasting() {
+
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0.0f));
+		Debug.DrawRay (ray.origin, ray.direction * 10, Color.cyan);
+		RaycastHit hit;
+
+		if (Input.GetMouseButtonUp (0)) {
+
+			if(Physics.Raycast(ray, out hit) == true)
+			{
+
+				Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+				Debug.Log("Ray hit a " + hit.transform.gameObject.tag);
+			}
+
 			cube.position = hit.point;
 			cube.rotation = Quaternion.LookRotation (hit.normal);
-//		}
+
+		}
 	}
 }
