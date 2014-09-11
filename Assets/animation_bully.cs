7ae08715f26cca64b1903075d68aa807715fa564
manmit singh <manmit.singh408@gmail.com>
2014-09-10 19:53:06 -0700

    fixed shooting w/animation

---
+++ Assets/animation_bully.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animation_bully : MonoBehaviour {
+	
+	void Start () {
+		animation.wrapMode =WrapMode.Loop;
+		
+		animation["idle"].wrapMode=WrapMode.Clamp;
+		animation["run"].wrapMode=WrapMode.Clamp;
+		animation["death"].wrapMode=WrapMode.Clamp;
+	}
+	
+	void Update () {
+		
+		animation.CrossFade("idle");
+		print("idle");;
+
+		if (Input.GetKey (KeyCode.A))
+			animation.Play ("run");
+		print ("run");
+	}
+}
